11350a0d2a85e712b159549fe892372df7953309
Nihal Borkar <nihalborkar2024.comp@mmcoe.edu.in>
2025-09-25 22:38:27 +0530

    Update Practical4.cpp

--- Practical4.cpp
+++ Practical4.cpp
@@ -1,1 +1,17 @@
+#include <iostream>  
+using namespace std;  
+long long factorial(int n) {  
+long long fact = 1;        
+//Initializing the fact 1  
+for(int i = 2; i <= n; i++) {    //Staring the fact from 2   
+fact *= i;  
+}  
+return fact;  
+}  
+int main() {  
+int n = 20, r = 3;      
+long long c = factorial(n) / (factorial(r) * factorial(n - r));   //formula for combination  
+cout << "Combination is: " << c << endl;   //output  
+return 0;  
+} 
 
