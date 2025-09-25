b5c63b61f7300bfded12344531db53d1fbcf34e5
Nihal Borkar <nihalborkar2024.comp@mmcoe.edu.in>
2025-09-25 22:37:49 +0530

    Update Practical3.cpp

--- Practical3.cpp
+++ Practical3.cpp
@@ -1,1 +1,22 @@
+//Permutation  
+#include <iostream>  
+using namespace std;  
+int fact(int a){  
+if(a==0||a==1){  
+return 1;  
+}else {  
+return a*fact(a-1);  
+}  
+}  
+int main(){  
+int n,r;      
+cout<<"Enter the total participants:";  
+cin>>n;  
+cout<<"Enter the no. of ways:";  
+cin>>r;  
+int p=fact(n)/fact(n-r);  
+cout<<"The permutation is:";  
+cout<<p;  
+return 0;  
+}
 
